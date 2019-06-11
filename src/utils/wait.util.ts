681f540b923d4e3ff4f3103ed1a0c1613f90b9bb
Cameron Householder <cameron.householder@ruralsource.com>
2019-06-11 08:02:34 -0500

    changed the naming convention

--- src/utils/wait.util.ts
+++
@@ -1,7 +0,0 @@
-import { browser, ElementFinder, protractor } from "protractor";
-
-export default class WaitUtil {
-    public async waitForElemToBeVisible(elem: ElementFinder) {
-        await browser.wait(protractor.ExpectedConditions.presenceOf(elem), 20000, elem.locator().toString());
-    }
-}
